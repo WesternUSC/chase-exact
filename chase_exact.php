4ebf5f3867de5d04d9fb7266ff0803641ad8ec10
Nikolas North <nikolasn09@gmail.com>
2021-01-13 11:48:29 -0500

    Add Chase Exact to WooCommerce gateways

--- chase_exact.php
+++ chase_exact.php
@@ -6,4 +6,17 @@
  * Author URI: https://github.com/WesternUSC
  * Version: 1.0.0
  */
-?>
+
+// Register Chase_Exact class as WooCommerce payment gateway
+add_filter('woocommerce_payment_gateways', 'add_chase_exact');
+function add_chase_exact($methods) {
+    $methods[] = 'WC_Chase_Exact';
+    return $methods;
+}
+
+add_action('plugins_loaded', 'init_chase_exact');
+function init_chase_exact() {
+    class WC_Chase_Exact_Gateway extends WC_Payment_Gateway {
+        // TODO
+    }
+}
