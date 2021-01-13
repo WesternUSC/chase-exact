a2e75588019699ddad5e80df860867dd0ff59c15
Nikolas North <nikolasn09@gmail.com>
2021-01-13 11:21:09 -0500

    Register plugin

---
+++ chase_exact.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name: WooCommerce Chase E-xact Payment Gateway
+ * Description: Process payments with Chase E-xact gateway.
+ * Author: Nikolas North (WesternUSC)
+ * Author URI: https://github.com/WesternUSC
+ * Version: 1.0.0
+ */
+?>
